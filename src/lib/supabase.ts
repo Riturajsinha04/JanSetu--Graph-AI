52741071e7f97ce913e4675f7e094712c286415f
Rituraj Sinha <riturajsinha040@gmail.com>
2026-07-14 23:20:32 +0530

    Fixed Supabase configuration and updated project

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl =  "https://gzuvwqosslryjcldzoyl.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6dXZ3cW9zc2xyeWpjbGR6b3lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3OTMwNjksImV4cCI6MjA5NzM2OTA2OX0.1g_8dY-QPXAODMEIMGZiO2ztYbKGBWlRJ7HZ8AKlpu8";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
